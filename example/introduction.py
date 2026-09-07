8ac4de631bd75d85fc7db5493092b091c6b817ca
William Bowley <wgrantbowley@gmail.com>
2026-09-08 05:14:27 +1000

    Literally insane work.

--- example/introduction.py
+++ example/introduction.py
@@ -6,7 +6,18 @@ Descriptions:
     Introduces the mechanics of picomats via a few examples
 """
 
+from picomats import mm
 from picomats import Materials
 
 
-bronze = Materials.abstract.alloy.bronze
+Materials.info()
+Materials.abstract.info()
+
+copper = Materials.abstract.pure.copper
+
+copper.info()
+density = copper.physical.density
+
+volume = 100 * mm ** 3
+mass = volume * density
+print(f"object mass: {mass}")
